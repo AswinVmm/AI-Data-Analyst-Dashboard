7fb2e61394370f0f0c09deb9a64eadf3982a4e25
AswinVmm <aswina002@gmail.com>
2026-04-07 07:12:27 +0530

    new change

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iDKjH1GzHz3P2xFkjuBxOenj0bI8e/ufptllTku6i+4=\"\n}"
--- frontend/.next/dev/static/chunks/src_app_page_09~7opn.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_axios_lib_0qhimsy._.js","static/chunks/node_modules_recharts_es6_util_11tj5a5._.js","static/chunks/node_modules_recharts_es6_state_0v6dt_m._.js","static/chunks/node_modules_recharts_es6_component_0_rw-7t._.js","static/chunks/node_modules_recharts_es6_cartesian_12pugth._.js","static/chunks/node_modules_recharts_es6_0xw54u9._.js","static/chunks/node_modules_0_bn4-q._.js","static/chunks/src_09v1xzb._.js"],
-    source: "dynamic"
-});
