964ed71a26b3dcd15c0b2d4a4402bf795c3c30e2
AswinVmm <aswina002@gmail.com>
2026-04-08 19:31:41 +0530

    chart

--- frontend/src/components/ChartView.js
+++ frontend/src/components/ChartView.js
@@ -1,5 +1,5 @@
 import { BarChart, Bar, XAxis, YAxis, Tooltip, LineChart, Line, PieChart, Pie, Cell, Legend } from "recharts";
-
+import { ResponsiveContainer } from "recharts";
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#AA336A"];
 
 export default function ChartView({ data }) {
@@ -28,26 +28,33 @@ export default function ChartView({ data }) {
         );
     }
     // 📊 BAR CHART
+
     if (data.type === "bar") {
         return (
-            <BarChart width={500} height={300} data={data.result}>
-                <XAxis dataKey="name" stroke="#555" />
-                <YAxis stroke="#555" />
-                <Tooltip contentStyle={{ backgroundColor: "#fff", borderRadius: "8px" }} />
-                <Bar dataKey="value" fill="#3b82f6" />
-            </BarChart>
+            <ResponsiveContainer width="100%" height={300}>
+                <BarChart data={data.result}>
+                    <XAxis dataKey="name" stroke="#555" interval={0} angle={-30}
+                        textAnchor="end" />
+                    <YAxis stroke="#555" />
+                    <Tooltip contentStyle={{ backgroundColor: "#fff", borderRadius: "8px" }} />
+                    <Bar dataKey="value" fill="#3b82f6" />
+                </BarChart>
+            </ResponsiveContainer>
         );
     }
 
     // 📈 LINE CHART
     if (data.type === "line") {
         return (
-            <LineChart width={500} height={300} data={data.result}>
-                <XAxis dataKey="name" stroke="#555" />
-                <YAxis stroke="#555" />
-                <Tooltip contentStyle={{ backgroundColor: "#fff", borderRadius: "8px" }} />
-                <Line type="monotone" dataKey="value" stroke="#10b981" strokeWidth={2} />
-            </LineChart>
+            <ResponsiveContainer width="100%" height={300}>
+                <LineChart data={data.result}>
+                    <XAxis dataKey="name" stroke="#555" interval={0} angle={-30}
+                        textAnchor="end" />
+                    <YAxis stroke="#555" />
+                    <Tooltip contentStyle={{ backgroundColor: "#fff", borderRadius: "8px" }} />
+                    <Line type="monotone" dataKey="value" stroke="#10b981" strokeWidth={2} />
+                </LineChart>
+            </ResponsiveContainer>
         );
     }
 
